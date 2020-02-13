dac70954d2f782cfc93a80b4f415948d61715ce6
Ever_lncr <roctivosu@gmail.com>
2020-02-13 06:11:06 -0500

    Edit docker-compose to run without tests

--- taskmanager/tasks/tests/tests.py
+++ taskmanager/tasks/tests/tests.py
@@ -1,7 +1,7 @@
 from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
-from .models import Tag, Task
+from tasks.models import Tag, Task
 
 
 class TaskTests(APITestCase):
